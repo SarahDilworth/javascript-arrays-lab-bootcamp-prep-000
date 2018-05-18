8756138817bce93fe044c770f886afddb2818d09
Sarah Dilworth <sarah.petty18@gmail.com>
2018-05-18 23:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+var kittens = ["Milo"[0] , "Otis"[1] , "Garfield"[2]];
+
+function destructivelyAppendKitten(){
+kittens.push("Ralph");
+return kittens;
+}
+
+function destructivelyPrependKitten(){
+kittens.unshift("Bob");
+return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+return kittens;
+}
+
+function appendKitten(){
+return [...kittens, "Broom"];
+}
+
+function prependKitten(){
+return ["Arnold", ...kittens];
+}
+
